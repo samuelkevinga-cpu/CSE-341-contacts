fa469d4861fdebbb8148429372ddde4876488228
samuelkevinga-cpu <samuelkevinga@gmail.com>
2026-09-11 12:49:20 -0600

    Adding routes GET, POST, PUT and DELETE

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,9 @@ const mongodb = require('../db/connect');
 const { ObjectId } = require('mongodb');
 
 const databaseName = process.env.MONGODB_DATABASE || 'cse341';
+const requiredFields = ['firstName', 'lastName', 'email', 'favoriteColor', 'birthday'];
+
+const hasAllFields = (body) => requiredFields.every((field) => body[field]);
 
 const getAll = async (req, res) => {
     try {
@@ -35,4 +38,68 @@ const getSingle = async (req, res) => {
     }
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+    if (!hasAllFields(req.body)) {
+        return res.status(400).json({ message: 'All contact fields are required.' });
+    }
+
+    try {
+        const result = await mongodb.getDb().db(databaseName).collection('contacts').insertOne(req.body);
+        res.status(201).json({ id: result.insertedId });
+    } catch (err) {
+        res.status(500).json({ message: 'Unable to create contact.' });
+    }
+};
+
+const updateContact = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid contact id.' });
+    }
+
+    if (!hasAllFields(req.body)) {
+        return res.status(400).json({ message: 'All contact fields are required.' });
+    }
+
+    try {
+        const result = await mongodb.getDb().db(databaseName).collection('contacts').replaceOne(
+            { _id: new ObjectId(req.params.id) },
+            req.body
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: 'Contact not found.' });
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).json({ message: 'Unable to update contact.' });
+    }
+};
+
+const deleteContact = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid contact id.' });
+    }
+
+    try {
+        const result = await mongodb.getDb().db(databaseName).collection('contacts').deleteOne({
+            _id: new ObjectId(req.params.id)
+        });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: 'Contact not found.' });
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).json({ message: 'Unable to delete contact.' });
+    }
+};
+
+module.exports = {
+    getAll,
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
+};
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const mongodb = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err) => {
     if (err) {
